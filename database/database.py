304ba0339cb7a256b9768407904542462712d6c5
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-02-21 05:29:20 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,18 +1,16 @@
-# (c) @AbirHasan2005 | X-Noid | @DC4_WARRIOR | WebXBots
-
-import datetime
 import motor.motor_asyncio
 
 class Database:
-
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.clinton = self._client[database_name]
-        self.col = self.clinton.USERS
+        self.db = self._client[database_name]
+        self.col = self.db.USERS
+
+    async def close(self):
+        self._client.close()  # Close connection when shutting down
 
     def new_user(self, id):
-        return dict(id=id, thumbnail=None)
-
+        return dict(id=int(id), thumbnail=None)
 
     async def add_user(self, id):
         user = self.new_user(id)
@@ -20,22 +18,21 @@ class Database:
 
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return True if user else False
+        return bool(user)
 
     async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
+        return await self.col.count_documents({})
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        return [user async for user in self.col.find({})]
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
+        await self.col.delete_one({'id': int(user_id)})  # Use delete_one instead of delete_many
 
     async def set_thumbnail(self, id, thumbnail):
-        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
+        await self.col.update_one({'id': int(id)}, {'$set': {'thumbnail': thumbnail}})
 
     async def get_thumbnail(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('thumbnail', None)
+        return user.get('thumbnail', None) if user else None
+        
